7049d4d239a48db2371af1a88627ab999b170862
Hassan Issa <hassan.issa2@outlook.com>
2022-06-16 22:44:48 -0400

    320px card component complete

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return(
+        <nav>
+            <img src={require('./images/logo.png')} alt="logo" width=""></img>
+            <h3>my travel journal.</h3>
+        </nav>
+    )
+}
