7776c387449a905049002ba7ff492b749114a8f3
Hassan Issa <hassan.issa2@outlook.com>
2022-06-17 16:25:15 -0400

    draft complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Card from './components/Card'
+import data from "./Data.js"
+
 
 export default function App() {
+  const vactionData = data.map(item => {
+    return (
+      <Card
+        title={item.title}
+        location={item.location}
+        googleMapsUrl={item.googleMapsUrl}
+        startDate={item.startDate}
+        endDate={item.endDate}
+        description={item.description}
+        imageUrl={item.imageUrl}
+      />
+    )
+  })
+
   return(
+
     <div>
         <Navbar />
-        <Card />
+        {vactionData}
     </div>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,26 @@
 import React from "react";
 
-export default function Card() {
+export default function Card(props) {
+    console.log(props)
+
     return(
-        <main className="main">
-            <img src="https://source.unsplash.com/WLxQvbMyfas" alt="" width=""></img>
-                <div className="main--top">
-                    <h5>Japan</h5>
-                    <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606237,138.7098538,14z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634" target="_blank" rel="noreferrer"
-                    >View on Google Maps</a>
-                </div>
-                <div className="main--bottom">
-                    <h1>Mount Fuji</h1>
-                    <h5>12 Jan, 2021 - 24 Jan, 2021</h5>
-                    <p>Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
-                    <div className="border"></div>
-                </div>
-        </main>
+        <section>
+            <main className="main">
+                <img className="main--img" src="https://source.unsplash.com/WLxQvbMyfas" alt="" width=""></img>
+                    <div className="main--break">
+                        <div className="main--top">
+                            <h5>{props.title}</h5>
+                            <a href="https://www.google.com/maps/place/Mount+Fuji/@35.3606237,138.7098538,14z/data=!3m1!4b1!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634" target="_blank" rel="noreferrer"
+                            >View on Google Maps</a>
+                        </div>
+                        <div className="main--bottom">
+                            <h1>{props.location}</h1>
+                            <h5>{props.startDate} - {props.EndDate}</h5>
+                            <p>{props.description}</p>
+                        </div>
+                    </div>
+            </main>
+         <div className="border"></div>
+        </section>
     )
 }
